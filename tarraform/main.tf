07f4ef6cacdc87a62610c5b21eb70ea813d7c30e
Kurbanalieva-Dev <kurbanalieva821@gmail.com>
2026-07-14 06:53:23 +0300

    Update main.tf

--- tarraform/main.tf
+++ tarraform/main.tf
@@ -1,29 +1,20 @@
-# 1. Указываем, где мы работаем
-provider "aws" {
-  region = "eu-north-1"
-}
-
-# 2. Создаем сеть (VPC), чтобы всё было изолировано
-resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = { Name = "Bank-VPC" }
-}
-
-# 3. Создаем "умную" Security Group
 resource "aws_security_group" "allow_ssh_http" {
   name        = "allow_traffic"
+  description = "Разрешаем доступ для SSH и Web" # Тот самый description, который просил Checkov
   vpc_id      = aws_vpc.main_vpc.id
 
-  # Разрешаем SSH (для Ansible)
+  # Разрешаем SSH (порт 22) только тебе
   ingress {
+    description = "SSH для админа"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["ТВОЙ_IP_ЗДЕСЬ/32"] # Вставь сюда свой реальный IP
   }
 
-  # Разрешаем HTTP (для твоего сайта)
+  # Разрешаем HTTP (порт 80) всем
   ingress {
+    description = "Сайт для всех"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
